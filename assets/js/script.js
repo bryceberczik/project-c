3048a42784bb4e9c5a5a9ee424d1ab2ecaa36250
bryceberczik <berczikbryce@gmail.com>
2024-08-02 22:30:30 -0500

    Completed application with reset feature and graph reseting

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,16 +17,16 @@ function renderInitialModal() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-    document.getElementById('captureForm').addEventListener('click', function() {
+    document.getElementById('captureForm').addEventListener('click', function () {
 
         userName = document.getElementById('nameInput').value;
         initialAmount = document.getElementById('initialAmountInput').value;
 
         const errorMessage = document.getElementById('error');
         const initialModal = bootstrap.Modal.getInstance(document.getElementById('initialModal'));
-        
+
         if (userName === '' || initialAmount === '') {
 
             errorMessage.textContent = `All fields are required. Please fill out the form completely.`;
@@ -39,12 +39,12 @@ document.addEventListener('DOMContentLoaded', function() {
             initialModal.hide();
         }
     });
-    
+
     let initialModalElement = document.getElementById('initialModal');
-    
+
     initialModalElement.addEventListener('hidden.bs.modal', function () {
 
-        
+
         let userName = localStorage.getItem('userName');
         let initialAmount = localStorage.getItem('initialAmount');
 
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    document.getElementById('submitIncome').addEventListener('click', function() {
+    document.getElementById('submitIncome').addEventListener('click', function () {
 
         const income = parseFloat(document.getElementById('incomeAmount').value);
         const subject = document.getElementById('incomeSubject').value;
@@ -73,7 +73,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const incomeList = document.getElementById('income-list');
 
         if (isNaN(income) || subject.trim() === '') {
-            
+
             errorMessage.textContent = `All fields are required. Please fill out the form completely.`;
         } else {
 
@@ -84,79 +84,85 @@ document.addEventListener('DOMContentLoaded', function() {
             let initialAmount = parseFloat(localStorage.getItem('initialAmount')) || 0;
             initialAmount += income;
             initialAmount = parseFloat(initialAmount).toFixed(2);
-        let incomes = JSON.parse(localStorage.getItem('incomes')) || [];
+            let incomes = JSON.parse(localStorage.getItem('incomes')) || [];
 
-        let newIncome = {
-            amount: income,
-            subject: subject
-        };
+            let newIncome = {
+                amount: income,
+                subject: subject
+            };
 
-        incomes.push(newIncome);
-        
+            incomes.push(newIncome);
 
-        localStorage.setItem('incomes', JSON.stringify(incomes));
-        localStorage.setItem('initialAmount', initialAmount.toString());
 
-        let incomeItem = document.createElement('p');
-        incomeItem.textContent = `+ ${subject}: $${income}`
-        incomeList.appendChild(incomeItem);
+            localStorage.setItem('incomes', JSON.stringify(incomes));
+            localStorage.setItem('initialAmount', initialAmount.toString());
 
-        let initialSpan = document.getElementById('initialSpan')
-        initialSpan.textContent = `Current balance: $${initialAmount}`;
+            let incomeItem = document.createElement('p');
+            incomeItem.textContent = `+ ${subject}: $${income}`
+            incomeList.appendChild(incomeItem);
 
-        incomeItem.style.color = '#68F553';
-        incomeItem.style.textAlign = 'center';
-        incomeItem.style.fontSize = '30px';
+            let initialSpan = document.getElementById('initialSpan')
+            initialSpan.textContent = `Current balance: $${initialAmount}`;
 
-        addIncomeModal.hide();
-        income.value = '';
-        subject.value = '';
+            incomeItem.style.color = '#68F553';
+            incomeItem.style.textAlign = 'center';
+            incomeItem.style.fontSize = '30px';
+
+            location.reload();
+
+            addIncomeModal.hide();
+            income.value = '';
+            subject.value = '';
         }
     })
 
-    document.getElementById('submitExpense').addEventListener('click', function() {
+    document.getElementById('submitExpense').addEventListener('click', function () {
 
         const expense = parseFloat(document.getElementById('expenseAmount').value);
         const subject = document.getElementById('expenseSubject').value;
         const errorMessage = document.getElementById('error2');
         const addExpenseModal = bootstrap.Modal.getInstance(document.getElementById('addExpenseModal'));
         const expenseList = document.getElementById('expense-list');
-    
+
         if (isNaN(expense) || subject.trim() === '') {
             errorMessage.textContent = `All fields are required. Please fill out the form completely.`;
         } else {
-    
+
             let allTimeExpense = parseFloat(localStorage.getItem('allTimeExpense')) || 0;
             allTimeExpense += expense;
             localStorage.setItem(`allTimeExpense`, allTimeExpense);
-    
+
             let initialAmount = parseFloat(localStorage.getItem('initialAmount')) || 0;
             initialAmount -= expense;
             initialAmount = parseFloat(initialAmount).toFixed(2);
             let expenses = JSON.parse(localStorage.getItem('expenses')) || [];
-    
+
             let newExpense = {
                 amount: expense,
                 subject: subject
             };
-    
+
             expenses.push(newExpense);
-    
+
             localStorage.setItem('expenses', JSON.stringify(expenses));
             localStorage.setItem('initialAmount', initialAmount.toString());
-    
+
             let expenseItem = document.createElement('p');
             expenseItem.textContent = `- ${subject}: $${expense}`;
             expenseList.appendChild(expenseItem);
-    
+
             let initialSpan = document.getElementById('initialSpan');
             initialSpan.textContent = `Current balance: $${initialAmount}`;
-    
+
             expenseItem.style.color = '#E07574';
             expenseItem.style.textAlign = 'center';
             expenseItem.style.fontSize = '30px';
-    
+
+            location.reload();
+
             addExpenseModal.hide();
+            expense.value = '';
+            subject.value = '';
         }
     });
 
@@ -171,17 +177,35 @@ document.addEventListener('DOMContentLoaded', function() {
             const noIncomeMessage = document.createElement('p');
             noIncomeMessage.textContent = 'No income added yet.';
             incomeList.appendChild(noIncomeMessage);
+
+            noIncomeMessage.style.textAlign = 'center';
+            noIncomeMessage.style.color = '#7482E1';
+            noIncomeMessage.style.fontSize = '30px';
         }
 
         if (!expenses || expenses.length === 0) {
             const noExpenseMessage = document.createElement('p');
             noExpenseMessage.textContent = 'No expense added yet.';
             expenseList.appendChild(noExpenseMessage);
+
+            noExpenseMessage.style.textAlign = 'center';
+            noExpenseMessage.style.color = '#7482E1';
+            noExpenseMessage.style.fontSize = '30px';
         }
     }
 
     checkLocalStorage();
 
+
+    resetLocalStorage.addEventListener('click', function () {
+        const resetModal = new bootstrap.Modal(document.getElementById('resetConfirmationModal'));
+        resetModal.show();
+    });
+
+    confirmReset.addEventListener('click', function () {
+        localStorage.clear();
+        location.reload();
+    });
 });
 
 function renderIncomes() {
@@ -201,7 +225,7 @@ function renderIncomes() {
         incomeItem.style.textAlign = 'center';
         incomeItem.style.fontSize = '30px';
     });
-    
+
 }
 
 function renderExpenses() {
@@ -221,122 +245,95 @@ function renderExpenses() {
         expenseItem.style.textAlign = 'center';
         expenseItem.style.fontSize = '30px';
     });
-    
+
 }
 
 function init() {
 
-        let userName = localStorage.getItem('userName');
-        let initialAmount = localStorage.getItem('initialAmount');
+    let userName = localStorage.getItem('userName');
+    let initialAmount = localStorage.getItem('initialAmount');
 
-        let userSpan = document.getElementById('userSpan');
-        let initialSpan = document.getElementById('initialSpan')
+    let userSpan = document.getElementById('userSpan');
+    let initialSpan = document.getElementById('initialSpan')
 
-        if (userName === null) {
-            userSpan.textContent = '';
-        } else {
-            userSpan.textContent = `Welcome, ${userName}.`;
-        }
+    if (userName === null) {
+        userSpan.textContent = '';
+    } else {
+        userSpan.textContent = `Welcome, ${userName}.`;
+    }
 
-        if (initialAmount === null) {
-            initialSpan.textContent = '';
-        } else {
-            initialSpan.textContent = `Current balance: $${initialAmount}`;
-        }
+    if (initialAmount === null) {
+        initialSpan.textContent = '';
+    } else {
+        initialSpan.textContent = `Current balance: $${initialAmount}`;
+    }
 
-        renderInitialModal();
-        renderIncomes();
-        renderExpenses();
+    renderInitialModal();
+    renderIncomes();
+    renderExpenses();
 }
 
 function updateButtonLabels() {
     const addIncomeBtn = document.getElementById('income-btn');
     const addExpenseBtn = document.getElementById('expense-btn');
-  
-    if (window.innerWidth <= 768) {
-      addIncomeBtn.textContent = '+';
-      addExpenseBtn.textContent = '-';
-    } else {
-      addIncomeBtn.textContent = 'Add Income';
-      addExpenseBtn.textContent = 'Add Expense';
-    }
-  }
-  
 
+    if (window.innerWidth <= 768) {
+        addIncomeBtn.textContent = '+';
+        addExpenseBtn.textContent = '-';
+    } else {
+        addIncomeBtn.textContent = 'Add Income';
+        addExpenseBtn.textContent = 'Add Expense';
+    }
+}
 
 let pieGraph = document.getElementById('pie-graph').getContext('2d');
-let polarGraph = document.getElementById('polar-graph').getContext('2d');
 
 let allTimeIncome = parseFloat(localStorage.getItem('allTimeIncome')) || 0;
 let allTimeExpense = parseFloat(localStorage.getItem('allTimeExpense')) || 0;
 
 let massPieChart = new Chart(pieGraph, {
-  type: 'pie',
-  data: {
-    labels: ['Income', 'Expenses'],
-    datasets: [{
-        label: 'Total',
-        data: [
-            allTimeIncome,
-            allTimeExpense
-        ],
-        backgroundColor: [
-            '#68F553',
-            '#E07574'
-        ]
-    }],
-  },
-  options: {
-    maintainAspectRatio: false
-  }
-});
-
-let massPolarChart = new Chart(polarGraph, {
-    type: 'polarArea',
+    type: 'pie',
     data: {
-      labels: ['Income', 'Expenses'],
-      datasets: [{
-          label: 'Total',
-          data: [
-              allTimeIncome,
-              allTimeExpense
-          ],
-          backgroundColor: [
-              '#68F553',
-              '#E07574'
-          ],
-      }],
+        labels: ['Income', 'Expenses'],
+        datasets: [{
+            label: 'Total',
+            data: [
+                allTimeIncome,
+                allTimeExpense
+            ],
+            backgroundColor: [
+                '#68F553',
+                '#E07574'
+            ]
+        }],
     },
     options: {
-      maintainAspectRatio: false,
+        maintainAspectRatio: false
     }
-  });
+});
 
-  // Get the button
 let scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-  scrollFunction();
+window.onscroll = function () {
+    scrollFunction();
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    scrollToTopBtn.style.display = "block";
-  } else {
-    scrollToTopBtn.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-scrollToTopBtn.addEventListener('click', function() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+scrollToTopBtn.addEventListener('click', function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 });
 
-  updateButtonLabels();
+updateButtonLabels();
 
-  
-  window.addEventListener('resize', updateButtonLabels);
+
+window.addEventListener('resize', updateButtonLabels);
 
 init();
